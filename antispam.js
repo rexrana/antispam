4d2083270dcbae902a46d2a038d32f5a19f5be48
Hideki Ito <pixture@270720.no-reply.drupal.org>
2010-03-28 17:26:33 +0000

    #569490 by Rob Loach: Applied patches #531122 by stephencarr: Fixed bugs that causes menu item disappear #721276 by kjholla #695668 by adresaklumea: Fixed a bug of broken comment links #658990 by MrGeek: Modified to obey permissions on submission Added API for other modules.

--- antispam.js
+++ antispam.js
@@ -1,37 +1,23 @@
-<!--
-function _antispam_switch_provider_js(myobj) {
-  wpapikey = document.getElementById('div-antispam-wpapikey');
-  tpapikey = document.getElementById('div-antispam-tpapikey');
-  deapikey = document.getElementById('div-antispam-deapikey');
-  if (myobj.value == 2) {
-    // defensio
-    deapikey.style.visibility = "visible";
-    deapikey.style.height = ""; // undefined
+// $Id$
 
-    wpapikey.style.visibility = "hidden";
-    wpapikey.style.height = 0;
-    tpapikey.style.visibility = "hidden";
-    tpapikey.style.height = 0;
-  }
-  else if (myobj.value == 1) {
-    // typepad antispam
-    tpapikey.style.visibility = "visible";
-    tpapikey.style.height = ""; // undefined
+/**
+ * @file
+ * Provides the administration JavaScript for the AntiSpam module.
+ */
 
-    wpapikey.style.visibility = "hidden";
-    wpapikey.style.height = 0;
-    deapikey.style.visibility = "hidden";
-    deapikey.style.height = 0;
-  }
-  else {
-    // akismet
-    wpapikey.style.visibility = "visible";
-    wpapikey.style.height = ""; // undefined
+/**
+ * The AntiSpam administration JavaScript behavior.
+ */
+Drupal.behaviors.antispam = function(context) {
+  // Hide everything but the active provider.
+  var active = $('input[name="antispam_service_provider"]:checked').attr('value');
+  $('.antispam-wrapper').hide();
+  $('#antispam-wrapper-' + active).show();
 
-    tpapikey.style.visibility = "hidden";
-    tpapikey.style.height = 0;
-    deapikey.style.visibility = "hidden";
-    deapikey.style.height = 0;
-  }
-}
-// -->
+  // Switch the active provider with user input.
+  $('input[name="antispam_service_provider"]').click(function() {
+    var active = $(this).attr('value');
+    $('.antispam-wrapper').hide();
+    $('#antispam-wrapper-' + active).show();
+  });
+};
