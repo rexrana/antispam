9b6dcfb4ae30db89ffdf92de439b57a6187aa530
Hideki Ito <pixture@270720.no-reply.drupal.org>
2011-01-03 18:24:25 +0000

    Initial release for Drupal 7.

--- antispam.js
+++ antispam.js
@@ -8,16 +8,20 @@
 /**
  * The AntiSpam administration JavaScript behavior.
  */
-Drupal.behaviors.antispam = function(context) {
-  // Hide everything but the active provider.
-  var active = $('input[name="antispam_service_provider"]:checked').attr('value');
-  $('.antispam-wrapper').hide();
-  $('#antispam-wrapper-' + active).show();
-
-  // Switch the active provider with user input.
-  $('input[name="antispam_service_provider"]').click(function() {
-    var active = $(this).attr('value');
-    $('.antispam-wrapper').hide();
-    $('#antispam-wrapper-' + active).show();
-  });
-};
+(function ($) {
+  Drupal.behaviors.antispam = {
+    attach : function(context, settings) {
+      // Hide everything but the active provider.
+      var active = $('input[name="antispam_service_provider"]:checked').attr('value');
+      $('.antispam-wrapper').hide();
+      $('#antispam-wrapper-' + active).show();
+  
+      // Switch the active provider with user input.
+      $('input[name="antispam_service_provider"]').click(function() {
+        var active = $('input[name="antispam_service_provider"]:checked').attr('value');
+        $('.antispam-wrapper').hide();
+        $('#antispam-wrapper-' + active).show();
+      });
+    }
+  };
+})(jQuery);
